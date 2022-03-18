2e48c5fb580a8de95142f57c3de7ccdd4dba59e7
Luis Puga <duhluis.p@gmail.com>
2022-03-18 15:59:28 -0400

    If applied, this commit will have the basic structure to our scanning function. It is currently not working.

---
+++ main.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class mainApp(App):
+    def build(self):
+        return Builder.load_string(
+"""
+#:import ZBarCam kivy_garden.zbarcam.ZBarCam
+BoxLayout:
+    orientation: 'vertical'
+    ZBarCam:
+        id:qrcodecam
+    Label:
+        size_hint: None, None
+        size: self.texture_size[0], 50
+        text: ' '.join([str(symbol.data) for symbol in qrcodecam.symbols])
+"""
+)
+
+if __name__ == '__main__':
+    mainApp().run()
